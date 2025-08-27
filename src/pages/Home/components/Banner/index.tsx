45786f18e459a9f859387575b42c5dbc6a6a267d
Doubley Yang <doubley_yang@epam.com>
2025-08-28 00:27:23 +0800

    style: update banner style

--- src/pages/Home/components/Banner/index.tsx
+++ src/pages/Home/components/Banner/index.tsx
@@ -15,35 +15,39 @@ const Banner: React.FC = () => {
   return (
     <div className={css.banner}>
       <div className={css.bannerContainer}>
-        <div>
+        <div className={css.bannerRecommend}>
           <div className={css.bannerContent}>
             <ImageBg className={css.bannerContentContainer} imageUrl={bannerUrls[0]}></ImageBg>
           </div>
         </div>
         <div>
-          <div className={css.bannerContent}>
-            <ImageBg className={css.bannerContentContainer} imageUrl={bannerUrls[1]} onClick={() => jumper.DOLLS()}>
-              <div>DOLLS</div>
-              <div><div></div></div>
-            </ImageBg>
+          <div className={css.bannerItems}>
+            <div className={css.bannerContent}>
+              <ImageBg className={css.bannerContentContainer} imageUrl={bannerUrls[1]} onClick={() => jumper.DOLLS()}>
+                <div>DOLLS</div>
+                <div><div></div></div>
+              </ImageBg>
+            </div>
+            <div className={css.bannerContent}>
+              <ImageBg className={css.bannerContentContainer} imageUrl={bannerUrls[2]} onClick={() => jumper.FACES()}>
+                <div>FACES</div>
+                <div><div></div></div>
+              </ImageBg>
+            </div>
           </div>
-          <div className={css.bannerContent}>
-            <ImageBg className={css.bannerContentContainer} imageUrl={bannerUrls[2]} onClick={() => jumper.FACES()}>
-              <div>FACES</div>
-              <div><div></div></div>
-            </ImageBg>
-          </div>
-          <div className={css.bannerContent}>
-            <ImageBg className={css.bannerContentContainer} imageUrl={bannerUrls[3]} onClick={() => jumper.TORSOS()}>
-              <div>TORSOS</div>
-              <div><div></div></div>
-            </ImageBg>
-          </div>
-          <div className={css.bannerContent}>
-            <ImageBg className={css.bannerContentContainer} imageUrl={bannerUrls[4]} onClick={() => jumper.ACCESSORIES()}>
-              <div>ACCESSORIES</div>
-              <div><div></div></div>
-            </ImageBg>
+          <div className={css.bannerItems}>
+            <div className={css.bannerContent}>
+              <ImageBg className={css.bannerContentContainer} imageUrl={bannerUrls[3]} onClick={() => jumper.TORSOS()}>
+                <div>TORSOS</div>
+                <div><div></div></div>
+              </ImageBg>
+            </div>
+            <div className={css.bannerContent}>
+              <ImageBg className={css.bannerContentContainer} imageUrl={bannerUrls[4]} onClick={() => jumper.ACCESSORIES()}>
+                <div>ACCESSORIES</div>
+                <div><div></div></div>
+              </ImageBg>
+            </div>
           </div>
         </div>
       </div>
