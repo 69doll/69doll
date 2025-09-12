9edfa6b56e3923d5b46ce2c144370b14cadefb14
Doubley Yang <doubley_yang@epam.com>
2025-09-12 08:56:01 +0800

    feat: support multi mode

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,42 @@
+import { useEffect, useMemo, useState } from "react"
+import { useLoaderData } from "react-router-dom"
 import { match } from "ts-pattern"
+import { compile, match as pMatch } from "path-to-regexp"
 import Banner from "./components/Banner"
 import LargeAD from "./components/LargeAD"
 import Recommend from "./components/Recommend"
 import { genLoaderData } from "../../data"
-import { useLoaderData } from "react-router-dom"
-import { useEffect, useMemo, useState } from "react"
 import useCurrentLanguage from "../../hooks/useCurrentLanguage"
 
 const VITE_API_DATA_DOMAIN = import.meta.env.VITE_API_DATA_DOMAIN
+const isProd = import.meta.env.MODE === 'production'
+const isSSR = import.meta.env.SSR
+
+const toPathRepeated = compile("{/:mode}{/*segment}")
+
+function transformsPathname (pathname: string) {
+  const mode = isProd ? undefined : import.meta.env.MODE
+  if (pathname === '/') {
+    if (mode) return `/${mode}`
+    return pathname
+  }
+  const newPathname = toPathRepeated({
+    mode: mode,
+    segment: (pMatch("/*splat")(pathname) as any).params?.splat ?? [],
+  })
+  return newPathname
+}
 
 async function getDataAsync<D>(lang: string | undefined, pathname: string) {
-  const url = new URL(pathname, VITE_API_DATA_DOMAIN).href
+  const url = new URL(transformsPathname(pathname), VITE_API_DATA_DOMAIN).href
   try {
-    console.log(url, '... fetching')
+    if (!isProd || isSSR) console.log(url, '... fetching')
     const data = await fetch(url)
     const mockData = await data.json()
-    console.error(url, '... fetch done')
+    if (!isProd || isSSR) console.log(url, '... fetch done')
     return genLoaderData<D>(lang, mockData)
   } catch (e) {
-    console.error(url, '... fetch fail')
+    if (!isProd || isSSR) console.error(url, '... fetch fail')
   }
   return genLoaderData<D>(lang, {})
 }
