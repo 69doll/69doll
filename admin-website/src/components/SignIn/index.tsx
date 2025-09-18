17a826368ee812070f4cb3b5c50f9fc6276f20b4
Doubley Yang <doubley_yang@epam.com>
2025-09-19 00:32:12 +0800

    feat(admin): new create user logic

--- admin-website/src/components/SignIn/index.tsx
+++ admin-website/src/components/SignIn/index.tsx
@@ -4,16 +4,19 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router";
 import Logo from "../Logo";
+import SHA1 from 'crypto-js/sha1'
 
 const SignIn: React.FC = () => {
   const nav = useNavigate()
   const login = async (formData: FormData) => {
+    const body = Object.fromEntries([...formData.entries()])
+    body.password = SHA1(body.password as string).toString()
     const res = await fetch('/api/admin/auth/login', {
       method: 'POST',
       headers: {
         'content-type': 'application/json'
       },
-      body: JSON.stringify(Object.fromEntries([...formData.entries()])),
+      body: JSON.stringify(body),
     })
     const data = await res.json()
     if (data.code === 200) {
--- admin-website/src/components/Users/index.tsx
+++ admin-website/src/components/Users/index.tsx
@@ -11,6 +11,7 @@ import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetT
 import { Label } from "../ui/label"
 import { Input } from "../ui/input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
+import SHA1 from 'crypto-js/sha1'
 
 const SUPPORT_PAGE_SIZE = [25, 50, 100]
 
@@ -37,8 +38,31 @@ const Users: React.FC = () => {
   }
 
   const [editUser, setEditUser] = useState<any>()
+  const onFormChange = ({ name, value }: { name: string, value: any }) => {
+    setEditUser({
+      ...editUser,
+      [name]: value,
+    })
+  }
   const isOpenSheet = useMemo(() => !!editUser, [editUser])
-  const { refetch: saveUser, reset } = useQuery('/api/admin/user/update', {
+  const { refetch: addUser, reset: resetAddStatus } = useQuery('/api/admin/user/create', {
+    method: 'POST',
+    body: {
+      email: editUser?.email,
+      nickname: editUser?.nickname,
+      status: !!editUser?.status,
+      type: userType,
+      rawPassword: SHA1(editUser?.rawPassword).toString(),
+    },
+    fetchOnMount: false,
+  })
+  const add = async () => {
+    await addUser()
+    setEditUser(undefined)
+    resetAddStatus()
+    await refetch()
+  }
+  const { refetch: updateUser, reset: resetUpdateStatus } = useQuery('/api/admin/user/update', {
     method: 'PUT',
     body: {
       id: editUser?.id,
@@ -47,19 +71,22 @@ const Users: React.FC = () => {
     fetchOnMount: false,
   })
   const save = async () => {
-    await saveUser()
+    await updateUser()
     setEditUser(undefined)
-    reset()
+    resetUpdateStatus()
     await refetch()
   }
   return <>
     <h1>Users</h1>
-    <Tabs defaultValue={userType} onValueChange={(v) => setUserType(v)}>
-      <TabsList>
-        <TabsTrigger value={SUPPORT_TYPE[0]}>普通用户</TabsTrigger>
-        <TabsTrigger value={SUPPORT_TYPE[1]}>管理员</TabsTrigger>
-      </TabsList>
-    </Tabs>
+    <div className='flex flex-row justify-between'>
+      <Tabs defaultValue={userType} onValueChange={(v) => setUserType(v)}>
+        <TabsList>
+          <TabsTrigger value={SUPPORT_TYPE[0]}>普通用户</TabsTrigger>
+          <TabsTrigger value={SUPPORT_TYPE[1]}>管理员</TabsTrigger>
+        </TabsList>
+      </Tabs>
+      <Button variant="outline" onClick={() => setEditUser({ status: true })}>新增用户</Button>
+    </div>
     <Table>
       <TableHeader>
         <TableRow>
@@ -73,7 +100,7 @@ const Users: React.FC = () => {
       <TableBody>
         <Doll69If display={isLoading}>
           {
-            Array(20).map(() => <TableRow>
+            Array(pageSize).map(() => <TableRow>
               <TableCell><Skeleton className="h-4 w-full" /></TableCell>
               <TableCell><Skeleton className="h-4 w-full" /></TableCell>
               <TableCell><Skeleton className="h-4 w-full" /></TableCell>
@@ -110,28 +137,43 @@ const Users: React.FC = () => {
     <Sheet open={isOpenSheet}>
       <SheetContent headerClose={false}>
         <SheetHeader>
-          <SheetTitle>修改用户信息</SheetTitle>
-          <SheetDescription>
-            当前正在修改用户[{`${editUser?.email}`}]的信息
-          </SheetDescription>
+
+          <Doll69If display={!editUser?.id}>
+            <SheetTitle>添加用户</SheetTitle>
+            <SheetDescription>
+              添加一个{userType === SUPPORT_TYPE[0] ? '普通用户' : '管理员'}
+            </SheetDescription>
+          </Doll69If>
+          <Doll69If display={editUser?.id}>
+            <SheetTitle>修改用户信息</SheetTitle>
+            <SheetDescription>
+              当前正在修改用户[{`${editUser?.email}`}]的信息
+            </SheetDescription>
+          </Doll69If>
         </SheetHeader>
-        <div className="grid auto-rows-min gap-6 px-4">
+        <form className="grid auto-rows-min gap-6 px-4" onChange={(a) => onFormChange(a.target as any)}>
           <div className="grid gap-3">
             <Label htmlFor="user-id">ID</Label>
-            <Input id="user-id" defaultValue={editUser?.id} readOnly={true} name='id' />
+            <Input id="user-id" defaultValue={editUser?.id} disabled={true} name='id' />
           </div>
           <div className="grid gap-3">
             <Label htmlFor="user-email">Email</Label>
-            <Input id="user-email" defaultValue={editUser?.email} readOnly={true} />
+            <Input id="user-email" defaultValue={editUser?.email} disabled={!!editUser?.id} name='email' />
           </div>
           <div className="grid gap-3">
             <Label htmlFor="user-nickname">昵称</Label>
             <Input id="user-nickname" defaultValue={editUser?.nickname} name='nickname' />
           </div>
+          <Doll69If display={!editUser?.id}>
+            <div className="grid gap-3">
+              <Label htmlFor="user-password">密码</Label>
+              <Input id="user-password" defaultValue={editUser?.rawPassword} name='rawPassword' type='password' />
+            </div>
+          </Doll69If>
           <div className="grid gap-3">
             <Label htmlFor="user-id">状态</Label>
-            <Select defaultValue={editUser?.status.toString()}>
-              <SelectTrigger className="w-[180px]">
+            <Select defaultValue={editUser?.status.toString()} disabled={!!editUser?.id} name='status'>
+              <SelectTrigger className="w-full">
                 <SelectValue />
               </SelectTrigger>
               <SelectContent>
@@ -140,9 +182,14 @@ const Users: React.FC = () => {
               </SelectContent>
             </Select>
           </div>
-        </div>
+        </form>
         <SheetFooter>
-          <Button type="submit" variant="outline" onClick={() => save()}>立即修改</Button>
+          <Doll69If display={!editUser?.id}>
+            <Button type="submit" variant="outline" onClick={() => add()}>立即添加</Button>
+          </Doll69If>
+          <Doll69If display={editUser?.id}>
+            <Button type="submit" variant="outline" onClick={() => save()}>立即修改</Button>
+          </Doll69If>
           <Button variant="outline" onClick={() => setEditUser(undefined)}>关闭</Button>
         </SheetFooter>
       </SheetContent>
