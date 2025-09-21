1c76195743db1d47c14431b9794947a0be0dce26
Doubley Yang <doubley_yang@epam.com>
2025-09-21 21:39:12 +0800

    refactor(admin): streamline login process by encapsulating mutation logic

--- admin-website/src/components/SignIn/index.tsx
+++ admin-website/src/components/SignIn/index.tsx
@@ -1,14 +1,15 @@
 import type React from "react";
-import css from './style.module.scss'
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import SHA1 from "crypto-js/sha1";
+import css from "./style.module.scss";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { useNavigate } from "react-router";
 import Logo from "../Logo";
-import SHA1 from 'crypto-js/sha1'
-import { useQuery } from "@tanstack/react-query";
 import { getCurrentUser, getCurrentUserCacheKeys } from "@/request/user";
 import { AuthProvider } from "@/provider/auth";
-import { useEffect } from "react";
+import type { ApiResBody } from "@/types/api.type";
 
 const SignIn: React.FC = () => {
   const nav = useNavigate()
@@ -23,20 +24,28 @@ const SignIn: React.FC = () => {
       nav('/')
     }
   }, [currentUser])
+  const { mutateAsync } = useMutation({
+    mutationFn: async (formData: FormData) => {
+      const body = Object.fromEntries([...formData.entries()])
+      body.password = SHA1(body.password as string).toString()
+      const res = await fetch('/api/admin/auth/login', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(body),
+      })
+      const data = await res.json() as ApiResBody
+      return data
+    },
+    async onSuccess (data) {
+      if (data.code === 200) {
+        await refetchCurrentUser()
+      }
+    },
+  })
   const login = async (formData: FormData) => {
-    const body = Object.fromEntries([...formData.entries()])
-    body.password = SHA1(body.password as string).toString()
-    const res = await fetch('/api/admin/auth/login', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(body),
-    })
-    const data = await res.json()
-    if (data.code === 200) {
-      await refetchCurrentUser()
-    }
+    mutateAsync(formData)
   }
   return <>
     <form action={login} className={css.container}>
